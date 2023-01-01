9a340cbc8c432c343326155645fafd5e66c00cc2
yuzu <1581523609@qq.com>
2023-01-01 14:34:14 +0800

    添加pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./plugins/router";
+import store from "./plugins/store";
 
 // 初始化样式
 import "@/style/reset.css";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+// 注册
+app.use(store);
+app.use(router);
+app.mount("#app");
