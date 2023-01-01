7e0cab731268844282af2e423843ca44a3ff4a0d
yuzu <1581523609@qq.com>
2023-01-01 22:02:00 +0800

    添加用户信息

--- src/plugins/store/modules/user.ts
+++ src/plugins/store/modules/user.ts
@@ -16,7 +16,20 @@ export const userStore = defineStore("userStore", {
     },
     accessToken: "",
   }),
-  getters: {},
-  actions: {},
+  getters: {
+    getUserInfo: (state: State) => state.userInfo,
+  },
+  actions: {
+    authLogin(res: LoginData): Promise<void> {
+      this.userInfo.account = res.account;
+      this.userInfo.password = res.password;
+      this.accessToken = res.accessToken;
+      return Promise.resolve();
+    },
+    logout() {
+      this.accessToken = "";
+      this.userInfo.password = "";
+    },
+  },
   persist: true,
 });
