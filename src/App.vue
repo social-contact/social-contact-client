537cb27ca26a2b0aadbcb1df4cd177c779629884
yuzu <1581523609@qq.com>
2023-01-01 22:01:36 +0800

    添加监听窗口退出

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,20 @@
   <router-view />
 </template>
 
+<script setup lang="ts">
+import { ipcRenderer } from "electron";
+import { onMounted } from "vue";
+
+import { userStore } from "@/plugins/store/modules/user";
+
+const userStoreRecord = userStore();
+
+onMounted(() => {
+  // 设置监听退出程序
+  ipcRenderer.on("window-close", () => {
+    userStoreRecord.logout();
+  });
+});
+</script>
+
 <style lang="scss"></style>
