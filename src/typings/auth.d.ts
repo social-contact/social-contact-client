cf0ed5706554c341cecdc761a58101674a1f4c2a
yuzu <1581523609@qq.com>
2023-01-02 19:40:43 +0800

    修改类型

--- src/typings/auth.d.ts
+++ src/typings/auth.d.ts
@@ -1,14 +1,15 @@
 interface LoginParams {
   account: string;
-  password: string;
+  password?: string;
 }
 
-interface LoginData extends LoginParams {
+interface Token {
   accessToken: string;
 }
+interface LoginData extends LoginParams, Token {}
 
 interface RegisterParams {
   account: string;
-  password: string;
-  checkpassword: string;
+  password?: string;
+  checkpassword?: string;
 }
