540cbd357f275d872599f6a8d77ed0c14151e41f
yuzu <1581523609@qq.com>
2023-01-02 19:41:30 +0800

    添加登录、注册接口

---
+++ src/api/auth.ts
@@ -0,0 +1,17 @@
+import request from "@/plugins/request/index";
+
+export function UserLogin(data: LoginParams): Promise<string> {
+  return request({
+    url: "/user/login",
+    method: "post",
+    data,
+  });
+}
+
+export function UserRegister(data: RegisterParams): Promise<null> {
+  return request({
+    url: "/user/register",
+    method: "post",
+    data,
+  });
+}
