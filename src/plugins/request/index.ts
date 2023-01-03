31ade62d7394398da1c2bf1801c00427f75ccb9b
yuzu <1581523609@qq.com>
2023-01-03 17:15:07 +0800

    添加请求失败提示

--- src/plugins/request/index.ts
+++ src/plugins/request/index.ts
@@ -41,6 +41,11 @@ instance.interceptors.response.use(
     return Promise.reject(new Error(res.message || "Error"));
   },
   (error: AxiosError) => {
+    ElMessage({
+      message: error.message,
+      type: "error",
+      duration: 2 * 1000,
+    });
     return Promise.reject(error);
   }
 );
