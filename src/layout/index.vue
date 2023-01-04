ca1808efb6f204d9f0dc5768a142a996718e085e
Yuzu-io <1581523609@qq.com>
2023-01-04 21:20:33 +0800

    编写 layout 页面

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,7 +1,56 @@
 <template>
-  <router-view />
+  <div class="layout">
+    <div class="layout-head">
+      <div class="layout-head__container">
+        <LayoutHead></LayoutHead>
+      </div>
+    </div>
+    <div class="layout-container">
+      <div class="layout-container-menu">
+        <LayoutMenu></LayoutMenu>
+      </div>
+      <div class="layout-container-content">
+        <router-view />
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LayoutHead from "./components/LayoutHead/index.vue";
+import LayoutMenu from "./components/LayoutMenu/index.vue";
+</script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+$layout-head-hight: 80px;
+
+.layout {
+  background-color: $global-currency-background-color;
+  .layout-head {
+    height: $layout-head-hight;
+  }
+
+  .layout-container {
+    height: calc(100vh - $layout-head-hight);
+
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+
+    &-menu {
+      width: 100px;
+      height: 100%;
+      box-sizing: border-box;
+      padding: $global-currency-padding;
+      padding-right: 0;
+    }
+
+    &-content {
+      flex: 1;
+      height: 100%;
+      padding: $global-currency-padding;
+      box-sizing: border-box;
+    }
+  }
+}
+</style>
